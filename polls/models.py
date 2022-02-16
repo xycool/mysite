2b2847c6364aa0390f21ddd028fac8b205c6205a
Ethan Ji <jchwche@gmail.com>
2022-02-16 22:00:22 +0900

    add logics for test

--- polls/models.py
+++ polls/models.py
@@ -8,6 +8,10 @@ class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
 
+    def was_published_recently(self):
+        now = timezone.now()
+        return now - datetime.timedelta(days=1) <= self.pub_date <= now
+    
     def __str__(self):
         return self.question_text
 
@@ -15,9 +19,6 @@ class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
-
-    def was_published_recently(self):
-        return self.pub_date >= timezone.now() -  datetime.timedelta(days=1)
 
     def __str__(self):
         return self.choice_text
